e141ac45ca50fe1d4fcaa22806d74524b77ccea4
Plastic Soul <gildc@yahoo.fr>
2012-04-29 21:19:10 +0200

    Need:
    - GetId3 - Tags Editor
    http://getid3.sourceforge.net/
    
    - Music Player - soundmanager2
    http://www.schillmania.com
    
    - Wordpress User Auth.
    
    In Progress...
    
    CK Editor
    ...

---
+++ elfinder.src_psoDD.php
@@ -0,0 +1,323 @@
+
+
+	
+	<script src="<?php echo $acc;?>jquery/jquery-1.7.2.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>jquery/jquery-ui-1.8.18.custom.min.js" type="text/javascript" charset="utf-8"></script>
+	<link rel="stylesheet" href="<?php echo $acc;?>jquery/ui-themes/smoothness/jquery-ui-1.8.18.custom.css" type="text/css" media="screen" title="no title" charset="utf-8">
+<!-- 
+	<script type="text/javascript" src="https://getfirebug.com/firebug-lite.js"></script>	
+
+<script type="text/javascript" src="http://localhost/personal-2012/soundupload/wp-soundupload/_includes/players/player-base-home/script/soundmanager2.js"></script>-->
+
+	<link rel="stylesheet" href="<?php echo $acc;?>css/common.css"      type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="<?php echo $acc;?>css/dialog.css"      type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="<?php echo $acc;?>css/toolbar.css"     type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="<?php echo $acc;?>css/navbar.css"      type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="<?php echo $acc;?>css/statusbar.css"   type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="<?php echo $acc;?>css/contextmenu.css" type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="<?php echo $acc;?>css/cwd.css"         type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="<?php echo $acc;?>css/quicklook.css"   type="text/css" media="screen" charset="utf-8">
+	
+<link rel="stylesheet" href="<?php echo $acc;?>css/commands.css"    type="text/css" media="screen" charset="utf-8">
+	
+	<link rel="stylesheet" href="<?php echo $acc;?>css/theme.css"       type="text/css" media="screen" charset="utf-8">
+	
+	<!-- elfinder core -->
+	<script src="<?php echo $acc;?>js/elFinder.js"           type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/elFinder.version.js"   type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/jquery.elfinder.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/elFinder.resources.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/elFinder.options.js"   type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/elFinder.history.js"   type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/elFinder.command.js"   type="text/javascript" charset="utf-8"></script>
+	
+	<!-- elfinder ui -->
+	<script src="<?php echo $acc;?>js/ui/overlay.js"       type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/workzone.js"      type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/navbar.js"        type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/dialog.js"        type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/tree.js"          type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/cwd.js"           type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/toolbar.js"       type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/button.js"        type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/uploadButton.js"  type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/viewbutton.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/searchbutton.js"  type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/sortbutton.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/panel.js"         type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/contextmenu.js"   type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/path.js"          type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/stat.js"          type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/ui/places.js"        type="text/javascript" charset="utf-8"></script>
+	
+	<!-- elfinder commands -->
+	<script src="<?php echo $acc;?>js/commands/back.js"      type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/forward.js"   type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/reload.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/up.js"        type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/home.js"      type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/copy.js"      type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/cut.js"       type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/paste.js"     type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/open.js"      type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/rm.js"        type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/info.js"      type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/duplicate.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/rename.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/help.js"      type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/getfile.js"   type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/mkdir.js"     type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/mkfile.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/upload.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/download.js"  type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/edit.js"      type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/quicklook.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/quicklook.plugins.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/extract.js"   type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/archive.js"   type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/search.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/view.js"      type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/resize.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/sort.js"      type="text/javascript" charset="utf-8"></script>	
+	<script src="<?php echo $acc;?>js/commands/netmount.js"      type="text/javascript" charset="utf-8"></script>	
+	<script src="<?php echo $acc;?>js/commands/pixlr.js"     type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/pso_play.js"     type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/commands/pso_idtag.js"     type="text/javascript" charset="utf-8"></script>
+
+	<!-- elfinder languages -->
+	<script src="<?php echo $acc;?>js/i18n/elfinder.ar.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.bg.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.ca.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.cs.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.de.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.en.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.es.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.fr.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.hu.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.jp.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.nl.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.pl.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.pt_BR.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.ru.js"    type="text/javascript" charset="utf-8"></script>
+	<script src="<?php echo $acc;?>js/i18n/elfinder.zh_CN.js" type="text/javascript" charset="utf-8"></script>
+
+	<!-- elfinder dialog -->
+	<script src="<?php echo $acc;?>js/jquery.dialogelfinder.js"     type="text/javascript" charset="utf-8"></script>
+
+	<!-- elfinder 1.x connector API support -->
+	<script src="<?php echo $acc;?>js/proxy/elFinderSupportVer1.js" type="text/javascript" charset="utf-8"></script>
+
+	<!-- elfinder custom extenstions -->
+	<script src="<?php echo $acc;?>extensions/jplayer/elfinder.quicklook.jplayer.js" type="text/javascript" charset="utf-8"></script>
+	
+	
+	
+	
+	<style type="text/css">
+		body { font-family: Arial, Helvetica, sans-serif;}
+		.button {
+			width: 100px;
+			position:relative;
+			display: -moz-inline-stack;
+			display: inline-block;
+			vertical-align: top;
+			zoom: 1;
+			*display: inline;
+			margin:0 3px 3px 0;
+			padding:1px 0;
+			text-align:center;
+			border:1px solid #ccc;
+			background-color:#eee;
+			margin:1em .5em;
+			padding:.3em .7em;
+			border-radius:5px; 
+			-moz-border-radius:5px; 
+			-webkit-border-radius:5px;
+			cursor:pointer;
+		}
+		
+/*		#dialog {
+			position:absolute;
+			left:50%;
+			top:1000px;
+		}
+*/		
+	</style>
+    <script type="text/javascript" charset="utf-8">
+	 // Helper function to get parameters from the query string.
+                        function getUrlParam(paramName) {
+                                var reParam = new RegExp('(?:[\?&]|&)' + paramName + '=([^&]+)', 'i') ;
+                                var match = window.location.search.match(reParam) ;
+                                
+                                return (match && match.length > 1) ? match[1] : '' ;
+                        }
+
+			// init 
+//CKEDITOR.init({});
+	</script>
+
+	<script type="text/javascript" charset="utf-8">
+		$().ready(function() {
+
+			// tinyMCE.init({});
+
+			$('#finder').elfinder({
+				// url : 'php/connector.php',
+					url : '<?php echo $acc;?>php/connector-full.php',
+					lang : 'fr',            // language (OPTIONAL)
+					rememberLastDir : false,
+					customData : { startPath : "<?php echo $customdirectory ?>" },
+					onlyMimes : '',
+					// uploadMaxSize : '800M',
+					width : 'auto',
+					dateFormat : 'd.m.Y H:m:i',
+					commandsOptions : {
+					
+					// configure value for "getFileCallback" used for editor integration
+					getfile : {
+						// send only URL or URL+path if false
+						onlyURL  : false,
+				
+						// allow to return multiple files info
+						multiple : false,
+				
+						// allow to return folders info
+						folders  : false,
+				
+						// action after callback (close/destroy)
+						oncomplete : ''
+					}},
+
+				// transport : new elFinderSupportVer1(),
+				// getFileCallback : function(files, fm) {
+				// 	console.log(files);
+				// },
+				// handlers : {
+				// 	select : function(e) {
+				// 		console.log(e.data)
+				// 	}
+				// },
+				// onlyMimes : ['image', 'text/plain']
+				//lang : 'fr',
+				//customData : {answer : 42},
+				// requestType : 'POST',
+				//rememberLastDir : false,
+	
+				// ui : ['tree', 'toolbar'],
+				// ui : ['toolbar', 'places', 'tree', 'path', 'stat'],
+				// commands : [],
+				// commandsOptions : {
+				// 	edit : {
+				// 		mimes : ['text/plain', 'text/html', 'text/javascript'],
+				// 		editors : [
+				// 			{
+				// 				mimes : ['text/html'],
+				// 				load : function(textarea) {
+				// 					tinyMCE.execCommand("mceAddControl", true, textarea.id);
+				// 				},
+				// 				close : function(textarea, instance) {
+				// 					tinyMCE.execCommand('mceRemoveControl', false, textarea.id);
+				// 				},
+				// 				save : function(textarea, editor) {
+				// 					textarea.value = tinyMCE.get(textarea.id).selection.getContent({format : 'html'});
+				// 					tinyMCE.execCommand('mceRemoveControl', false, textarea.id);
+				// 				}
+				// 
+				// 			}
+				// 		]
+				// 	}
+				// }
+				// uiOptions : {
+				// 	toolbar : [['help']]
+				// }
+			})
+
+			// $('#finder2').elfinder({
+			// 	url : 'connectors/php/connector.php',
+			// 	width:650,
+			// 	lang : 'ru'
+			// })
+
+			// console.log(f1)
+			// 
+			// setTimeout(function() {
+			// 	console.log($('#finder').elfinder('instance'))
+			// }, 2000)
+
+			// new elFinder($('<div/>'))
+
+			// var f2 = $('#asd').elfinder({
+			// 	url : 'connectors/php/connector.php',
+			// 	lang : 'ru'
+			// }).elfinder('instance')
+			
+			$('#back').click(function(e) {
+				f1.exec('back')
+			})
+			$('#fwd').click(function(e) {
+				f1.exec('forward')
+			})
+			
+			$('#dialog').click(function() {
+				
+				var fm = $('<div/>').dialogelfinder({
+					url : '<?php echo $acc;?>php/connector-full.php',
+					lang : 'fr',
+					width : 840,
+					destroyOnClose : true,
+					getFileCallback : function(files, fm) {
+						console.log(files);
+					},
+					commandsOptions : {
+						getfile : {
+							oncomplete : 'close',
+							folders : true
+						}
+					}
+				}).dialogelfinder('instance')
+
+			})
+			
+		})
+	
+	
+
+			
+	</script>
+
+<!-- 	<div id="dialog" class="button">dialog</div>
+
+<div id="back" class="button">back</div>
+	<div id="fwd" class="button">forward</div>
+		<div id="close"  class="button">close</div>
+	<div id="dock" class="button">dock</div>
+	<div id="undock" class="button">undock</div>
+	<div style="width:670px; float:left"> -->
+<div id="el"  style="display:noneo">		<div style="display:noneo" id="finder">finder <span>here</span></div>
+</div>
+<?php // echo 'ici'.$toz.$_SESSION['login'].'cookie'.$_COOKIE['UserSU'];?>
+<script>
+function toggleDivElf2() {
+
+
+  $('#finder').slideToggle(1500, function() {
+    // Animation complete.
+});}
+	</script>	<!-- </div> -->
+	<!-- <div style="width:670px; float:left">
+		<div id="finder2">finder</div> 
+	</div> 
+	<br clear="all"/>-->
+	<!-- <div><input type="text" id="i1"></div> -->
+	<!-- <div><input type="text" id="i2"></div>
+<span class="cont"></span> 
+
+<div id="playlistout" style="border: 1px solid rgb(204, 204, 204); margin: 5px 0pt; padding: 5px;"><ul id="play"></ul></div>-->
+<?php 
+//$args_pso =  array('target' => true, 'mimes' => false);
+//open('test');
+// $hash = '../../files/gildc/arrow2_e.png';
+// echo realpath($hash);
+//hashes($hash);
+//include_once('photo-manager.php'); ?>
+
